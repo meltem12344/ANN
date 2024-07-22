ad230a9013499220ba0b63c0c2857f63640252ff
Meltem Orhan <111740556+meltem12344@users.noreply.github.com>
2024-07-22 16:47:55 +0300

    ANN.py

--- ANN.py
+++ ANN.py
@@ -1,58 +1,84 @@
-import numpy as np  #bilimsel hesaplamalar için özellikle veri manipülasyonu ve olusturma işlemlerinde
-import matplotlib.pyplot as plt #grafik çizim kütüphansei  modelın kaybının gorsellestırılmesı ıcın kullanılack
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-import tensorflow as tf  #derın ogrenme kutuphanesı burdan derın ogrenme modellerı olusturup egıtıcez
-from tensorflow.keras import layers #layers modulunu sınır agı katmanları olusturmak ıcın kullanıca<
+df = pd.read_csv("campaign_data.csv")  # pandas kütüphanesi aracılığıyla veriler okunur
+print(df.head()) # kontrol için pandas tan head fonksiyonu ile verinin ilk bir kaç satırı döndürülür(özellikler gösterilir)
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data() #MNIST veri setini yükledik.60000 train verisi ve 10000 test verisi var. pixeli 28*28
-x_train = x_train/255
-x_test = x_test/255  #gorutnu verılerını 0 ile 1 arasında normalıze eder
-#fikir şu verı setınde goruntuler 28*28 piksel boyutunda gelıyor her pıksel degerı 0 ıle 255 arasında bır tam sayıdır bu degerlerı 255 e bolerek her degerı 0 ile 1 arasında bır degere donusturur
-#daha hızlı öğrenmesine ve daha iyi genelleştirme 
+df.drop(columns = ['CampaignID','RaisedAmount'], inplace= True ) # pandas tan drop fonksiyonu ile işe yaramayan özellikler çıkartıldı
+print(df.head())                                                   # peki işe yaramayanı hangi referansa göre belirledin?
+                                                                 # analizini yapmak istediğim şey şu: ! belirli özelliklere göre destekleyen kişi sayısını tahmin etmek !
 
-"""print(x_train.shape) # bu verilerin nasıl yapılandırıldığına bakmamamızı .shape saglar.                                                                        
-                    #cıktı:(60000,28,28) 28*28 pıksellık 60000 verı var
-                    #sunu fark et burda 3 tane ozellıgı var yanı bu 3 boyutlu bır nesnedır 
-print(y_train.shape)  # bununciktisi (60000) yanı bu kadar label oldugunu gosterır  
+print(df.isnull().sum()) # yine pandastan isnull fonksiyonunu çağırıyoruz, bu fonksiyon df nin her bir hücresinde eksik değer olup olmadığını sorgular,
+                         # eksikse true eksik değilse false değeri döndürür isnull, sum fonksiyonu True değerlerini 1 olarak sayar ve her sütub için eksik değerlerin toplamını verir
+                         # hepsinin değeri 0 çıktı, yani her şey normal eksik hücre bulunmuyor devam edilebilir
+                         # peki, eğer birinde 1 değeri dönerse ne olur? demekki bir hücre boş, bu eksik veriyi silebiliriz::satır silmek için::[df.dropna(inplace=True)]::sütün silmek için::[df.drop(columns=['column_name'], inplace=True)], doldurabiliriz::[df.fillna(value=0, inplace=True)]
+print(df.describe()) # count: Her sütundaki geçerli (eksik olmayan) veri sayısını gösterir.
+                     # mean: Her sütunun ortalama değerini hesaplar.
+                     # std: Her sütunun standart sapmasını hesaplar.
+                     # min: Her sütunun minimum değerini gösterir.
+                     # 25% (1. çeyrek): Verilerin yüzde 25'inin altında olduğu değeri gösterir.
+                     # 50% (medyan): Verilerin yüzde 50'sinin altında olduğu değeri gösterir.
+                     # 75% (3. çeyrek): Verilerin yüzde 75'inin altında olduğu değeri gösterir.
+                     # max: Her sütunun maksimum değerini gösterir.
+print(df.info())  # boş hücreleri, boş olmayan hücre sayısını vererek belirtir
+                  # veri tipini döndürür
 
-plt.imshow(x_train[0], cmap='Greys') # 0. ındextekı goruntuyu gorsellıstırme 
-                    #yukarıda x_train 3 boyutlu bı nesnedır dedık ve x_train[0] ifadesi ile elde edılen şey ıkı boyutlu bı nesne oluyır .28*28 bıyutunda bı nesne                    
-plt.show() """               
+print(df["Country"].value_counts()) # value_counts fonksiyonu, seçtiğimiz bir özelliğin farklı değerlerinin sayısını çıkartır
 
-
-#asagıdakı sınır agı modelını olusturmak ıcın kullanılır.modelın katmanlarını sıralı bır sekılde olusturmak ıcın kullanılır
-#her bır katman yalnızca bır oncekı katmanın cıktısını alır. basit ve sıralı veri işleme için uygun
-model_lr = tf.keras.models.Sequential([
-    layers.Input(x_train.shape[1:]),
-    layers.Flatten(),#çok boyutlu gırdı versını tek boyutlu bır vektore donusturur.
-                     #ve burda 28x28 piksellik bir görüntüyü 784 boyutlu bir vektöre dönüştürur
-                     #sonrakı fully coneccted katmanlarda gereklıdır.
-    layers.Dense(10, activation='softmax')#bu katmanda 10 noron var  yanı 10 cıkıs uretıcek. sınıf sayısı kadar noron genellıkle.
-                                          #her sınıfın olasılık degerını uretır.
-])                                        #ozetle:. dense katmanı flatten katmanının duzlestırdıgı verıyı(28*28=784 boyutlu) alır 10 cıktı degerıne donusturur 
-   #Dense katmanı tam baglı fully connected bır katmandır. bunun olayı şu her bir noronun bır oncekı katmandakı tum noronlara baglı oldugu anlamına gelır.                    
-   #bu katmanın parametrelerı : units(noron sayısı), activation(aktıvasyon fonksiyonu , input_shape(gırdı seklı))               
-   #                        katmandakı noron sayısı(ne kadar noron varsa o kadar cıkıs uretıcek), noronların cıksıını donusturmek ıcın kullanılan fonksıyon(agın ogrenme yetenegını artırır),ilk katmanda kullanılır ve gıtfı verısının seklını belırtır.
-   #                   (sınıflandırma problemlerinde sınıf sayısı kadar nöron kullanılır)                    
-   #                    (softmax bu bır aktıvasyon fonksıyonu  ve cok sınıflı sınıflandırma problemlerınde kullanılır. her bır sınıfın olasılık degerını uretir
-   #                    ()
-model_lr.compile(     #egitim baslamadan once gereklı yapılandırmaları yapar
-    optimizer='adam', #optımızasyon algorıtması
-    loss='categorical_crossentropy',#kayıp fonksıyonu  gercek etıketler ıle model tahmınlerı arasındakı farkı olceer:loss
-    metrics=['accuracy'])  #performans metrıgı   
-model_lr.summary()                
-
-y_onehot_train = tf.one_hot(y_train, 10)# Bir veri kümesindeki her sınıf, tüm sınıflar için ayrı bir pozisyonda '1' değerine sahip ve diğer pozisyonlarda '0' değerine sahip bir vektörle temsil edilir. <== onehot bunu saglıyor
-history_lr = model_lr.fit(x_train, y_onehot_train, epochs=10, batch_size=128, validation_split=0.2, verbose=False)  #modelı egıtım verılerı x_train yanı  ve one-hot encoded etıketlerı yanı y_onehot_train ile egitir                     
-                                   #kac kez verı uzerınden gececegını belırtır :epochs: daha fazla epochs daha ıyı ogrenme ama bbı noktada overflıttıng de olabılır!
-                                   #her bır guncelleme ıcın kac ornek kullanılacak :batch_size: daha kucuk batch_size daha sık guncelleme yapar ama egıtım suresını uzatabılır
-                                   #egıtım verısının bır kısmını dogrulama ıcın ayrılmasını saglar :validation_split:       
-#print(history_lr.history)  bununla accuracy loss val accuracy val loss bunların degerlerı verılır                 
+# görselleştirme
+plt.figure(figsize=(10, 6))
+df['Country'].value_counts().plot(kind='bar')
+plt.xlabel('Counrties Names')
+plt.ylabel('Numbers of Campaign')
+plt.title("Comparsion betweeen Countries and Campaign")
+plt.savefig('output.png')
+from IPython.display import Image, display
+display(Image(filename='output.png'))
 
 
-y_onehot_test = tf.one_hot(y_test, 10)
-print(model_lr.evaluate(x_test, y_onehot_test)) #burda test verılerını onehot ile dounusturmeden once calıstırdım valueerro verdı    
+df= pd.get_dummies(df, columns= ['Category', 'LaunchMonth', 'Country', 'Currency', 'VideoIncluded'], drop_first=True) # get_dummies kategorik verileri sayıya dönştürür
+                                                                          # kategorik değişkenlerin her bir benzersiz değeri için bir sütun oluşturur ve bu sütunlara 1 veya 0 değerleri atar
+                                                                          # "one_hot_encoding" denir buna
+print(df.head())                                                          # yeni veri ayrımını yaptık, çıktıda şu göze çarpıyor: artık 31 sütun var. neden? kategorik verileri makinanın anlayabileceği değerlere dönüştürmke için belirli özellikleri parçalara ayırıp True False değerlerine atadık
 
-print(model_lrpredict(x_test[:5]))
 
-                                                        
+
+X=df.drop(columns=['IsSuccessful']) # modelin özellikleri için yeni bir DataFrame(X)
+y=df['IsSuccessful']                # hedef değişkeni seri olarak saklar(y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.2, random_state=3) # veri kümesini test ve eğitim setlerine ayırıyoruz
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler() # özellikleri ölçeklendirdik
+X_train=sc.fit_transform(X_train) # eğitim veri kümesinin ortalama ve standart sapmasını hesaplar ve bnlarla x_train i standartlaştırır, en sonda her bır özellıgın ortalaması 0 standart sapması 1 olarak ayarlanır
+X_test=sc.transform(X_test)  # eğitim verilerinde hesaplanan ortalama ve standart sapmayı kullanarak X_test veri kümesini standartlaştırır
+print(X_train)
+print(y_train)
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential # ANN modelini başlatmak için kullanılır
+from tensorflow.keras.layers import Dense, InputLayer # farklı katman yapısı için kullanılırr
+
+classifier = Sequential()
+
+classifier.add(InputLayer(input_shape=(X_train.shape[1],)))
+
+classifier.add(Dense(50,activation='relu'))
+classifier.add(Dense(50,activation='relu'))
+classifier.add(Dense(50,activation='relu'))
+classifier.add(Dense(50,activation='relu'))
+classifier.add(Dense(1,activation='sigmoid'))
+print(classifier.summary())
+
+
+
+classifier.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),loss='binary_crossentropy',metrics=['accuracy'])
+
+
+history = classifier.fit(X_train,y_train,batch_size=32,epochs=100,verbose=1,validation_split=0.25)
+
+
+   
